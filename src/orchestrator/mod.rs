38ea9b2cf01e2114cdd84f1c16d5aba1d51b19b6
guzman-raphael <raphael.h.guzman@gmail.com>
2025-01-14 13:18:55 +0000

    Stub remaining orchestrator methods.

--- src/orchestrator/mod.rs
+++ src/orchestrator/mod.rs
@@ -23,21 +23,42 @@ pub trait Orchestrator {
     ///
     /// Will return `Err` if there is an issue querying metadata from containers.
     fn start(&self) -> Result<(), Box<dyn Error>>;
+    /// How to start containers with alternate image.
+    ///
+    /// # Errors
+    ///
+    /// Will return `Err` if there is an issue querying metadata from containers.
+    // fn start_with_altimage(&self) -> Result<(), Box<dyn Error>>;
+    /// How to load an image.
+    ///
+    /// # Errors
+    ///
+    /// Will return `Err` if there is an issue querying metadata from containers.
+    // fn load(&self) -> Result<(), Box<dyn Error>>;
     /// How to delete containers.
     ///
     /// # Errors
     ///
     /// Will return `Err` if there is an issue querying metadata from containers.
     fn delete(&self) -> Result<(), Box<dyn Error>>;
+    /// How to get container logs.
+    ///
+    /// # Errors
+    ///
+    /// Will return `Err` if there is an issue querying metadata from containers.
     // fn get_logs(p: &PodRun) -> Result<String, OrchestrationError>;
-    // fn list() -> Result<BTreeMap<String, Vec<String>>>;
+    /// How to get resource usage for container.
+    ///
+    /// # Errors
+    ///
+    /// Will return `Err` if there is an issue querying metadata from containers.
+    // fn metrics() -> OrchestratorMetrics;
     /// How to query containers.
     ///
     /// # Errors
     ///
     /// Will return `Err` if there is an issue querying metadata from containers.
     fn list(&self) -> Result<(), Box<dyn Error>>;
-    // fn metrics() -> OrchestratorMetrics;
 }
 
 // struct OrchestratorMetrics {
