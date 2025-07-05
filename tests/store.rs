21c12a60e2a1f81523eb02ecbe93f9c537abcceb
guzman-raphael <raphael.h.guzman@gmail.com>
2025-07-05 20:57:40 +0000

    Fix test.

--- tests/store.rs
+++ tests/store.rs
@@ -296,6 +296,11 @@ fn pod_annotation_unique() -> Result<()> {
         store.list_pod()?,
         vec![
             ModelInfo {
+                name: None,
+                version: None,
+                hash: new_hash,
+            },
+            ModelInfo {
                 name: Some(original_annotation.name.clone()),
                 version: Some(original_annotation.version.clone()),
                 hash: original_hash.clone(),
@@ -304,11 +309,6 @@ fn pod_annotation_unique() -> Result<()> {
                 name: None,
                 version: None,
                 hash: original_hash,
-            },
-            ModelInfo {
-                name: None,
-                version: None,
-                hash: new_hash,
             },
         ],
         "Pod list didn't return 3 expected entries."
