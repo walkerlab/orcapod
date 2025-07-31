24d167dae2f2c027f68de3f290462b63bebd17b9
guzman-raphael <raphael.h.guzman@gmail.com>
2025-07-31 13:17:44 +0000

    Replace finite cartesian_product in favor of multi_cartesian_product.

--- src/core/operator.rs
+++ src/core/operator.rs
@@ -1,6 +1,6 @@
 use crate::uniffi::{error::Result, model::packet::Packet};
 use itertools::Itertools as _;
-use std::{clone::Clone, collections::HashMap};
+use std::{clone::Clone, collections::HashMap, iter::IntoIterator};
 
 pub trait Operator {
     fn next(&mut self, packets: Vec<(String, Packet)>) -> Result<Vec<Packet>>;
@@ -24,32 +24,31 @@ impl JoinOperator {
 impl Operator for JoinOperator {
     fn next(&mut self, packets: Vec<(String, Packet)>) -> Result<Vec<Packet>> {
         let mut next_packets = vec![];
-        for (stream, packet) in packets {
-            let other_streams_iter = self
-                .received_streams
-                .iter()
-                .filter(|(other_stream, _)| *other_stream != &stream);
-            if other_streams_iter.clone().count() + 1 == self.parent_count {
-                let mut current_packets =
-                    other_streams_iter.fold(vec![packet.clone()], |total, (_, other_packets)| {
-                        total
-                            .iter()
-                            .cartesian_product(other_packets.iter())
-                            .map(|(left, right)| {
-                                let mut new = HashMap::new();
-                                new.extend(left.clone());
-                                new.extend(right.clone());
-                                new
-                            })
-                            .collect()
-                    });
-                next_packets.append(&mut current_packets);
+        for (stream, packet) in &packets {
+            if self.parent_count - usize::from(!self.received_streams.contains_key(stream))
+                == self.received_streams.len()
+            {
+                let packets_to_multiplex = self
+                    .received_streams
+                    .iter()
+                    .filter_map(|(parent_stream, parent_packets)| {
+                        (parent_stream != stream).then_some(parent_packets.clone())
+                    })
+                    .chain(vec![vec![packet.clone()]].into_iter());
+                let current_packets = packets_to_multiplex.multi_cartesian_product().map(
+                    |packet_combinations_to_merge| {
+                        packet_combinations_to_merge
+                            .into_iter()
+                            .flat_map(IntoIterator::into_iter)
+                            .collect::<HashMap<_, _>>()
+                    },
+                );
+                next_packets.extend(current_packets);
             }
-            if let Some(existing_packets) = self.received_streams.get_mut(&stream) {
-                existing_packets.push(packet);
-            } else {
-                self.received_streams.insert(stream, vec![packet]);
-            }
+            self.received_streams
+                .entry(stream.clone())
+                .or_default()
+                .push(packet.clone());
         }
         Ok(next_packets)
     }
