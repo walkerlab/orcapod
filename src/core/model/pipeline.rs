595a12e91e09da05f76d1491ff1e92a8e5ffbdec
Synicix <synicix@gmail.com>
2025-08-10 13:15:46 +0000

    Split model in core to match uniffi

--- src/core/model/pipeline.rs
+++ src/core/model/pipeline.rs
@@ -1,7 +1,7 @@
 use crate::uniffi::model::pipeline::Kernel;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct PipelineNode {
     pub name: String,
     pub kernel: Kernel,
