39b3996010a218b217378e861ab86bc8e5f9dca9
synicix <synicix@gmail.com>
2025-09-05 03:04:03 +0000

    Update excessive nesting exception for clippy

--- src/core/pipeline_runner.rs
+++ src/core/pipeline_runner.rs
@@ -750,6 +750,7 @@ impl PodProcessor {
     }
 }
 
+#[expect(clippy::excessive_nesting, reason = "Nesting manageable")]
 #[async_trait]
 impl NodeProcessor for PodProcessor {
     async fn process_incoming_packet(
@@ -845,6 +846,7 @@ impl<T: Operator + Send + Sync + 'static> OperatorProcessor<T> {
     }
 }
 
+#[expect(clippy::excessive_nesting, reason = "Nesting manageable")]
 #[async_trait]
 impl<T: Operator + Send + Sync + 'static> NodeProcessor for OperatorProcessor<T> {
     async fn process_incoming_packet(
