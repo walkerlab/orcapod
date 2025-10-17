dd5c44fc627a7eee2414a8ce2a956f6189b3ace2
synicix <synicix@gmail.com>
2025-10-17 01:23:37 +0000

    Remove clippy except since github action complained

--- src/core/pipeline_runner.rs
+++ src/core/pipeline_runner.rs
@@ -832,7 +832,6 @@ impl<T: Operator + Send + Sync + 'static> OperatorProcessor<T> {
     }
 }
 
-#[expect(clippy::excessive_nesting, reason = "Nesting is manageable here")]
 #[async_trait]
 impl<T: Operator + Send + Sync + 'static> NodeProcessor for OperatorProcessor<T> {
     async fn process_incoming_packet(
